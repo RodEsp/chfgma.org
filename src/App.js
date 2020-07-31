b6b56d5765a0af7e6fe445b8152b2bb76195b373
William Heiderman <heiderman.william@gmail.com>
2020-07-31 13:09:23 -0400

    router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import Header from './components/layout/Header.js';
 import Content from './components/layout/Content.js'
 import Footer from './components/layout/Footer.js';
 import './App.css';
 
+const routesMetadata = {
+  "/": {
+    backgroundColor: "rgb(241, 253, 247)",
+  },
+  "/about": {
+    backgroundColor: "rgb(241, 253, 247)",
+  },
+  "/get-help": {
+    backgroundColor: "rgb(247, 246, 255)",
+  },
+  "default": {
+    backgroundColor: "rgb(256, 256, 256)",
+  }
+};
 
-const App = () => {
+const App = ({ history }) => {
+  // console.debug("History :::", history)
+  const [style, setStyle] = useState(routesMetadata.default)
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // console.debug("EFFECT - History :::", history)
+    const currentRouteStyle = routesMetadata[history.location.pathname]
+    if (currentRouteStyle) {
+      setStyle(currentRouteStyle)
+    } else {
+      console.error("Error while setting background color: invalid history location")
+    }
+  })
+
   return (
     <div id="app-container">
-      <Router>
-        <Header />
-        <Content />
-        <Footer />
-      </Router>
+      <Header routesMetadata={routesMetadata} style={style} history={history} />
+      <Content />
+      <Footer />
     </div>
   );
 }
 
+App.propTypes = {
+  history: PropTypes.shape({
+    location: PropTypes.shape({
+      pathname: PropTypes.string.isRequired
+    })
+  }),
+}
 
 
-export default App;
+export default withRouter(App);
