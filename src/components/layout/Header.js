e36bd6ac2a6ace9b333bcb71b5200e6b14dbaa71
William Heiderman <heiderman.william@gmail.com>
2020-08-05 13:25:20 -0400

    basic responsive sticky nav

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -6,18 +6,44 @@ import "./Header.css"
 import magnifyingGlassSVG from "../../assets/icons/icon-magnifying-glass.svg"
 
 // TODO: responsive header for mobile with hamburger
- 
+
+function ResponsiveHeader() {
+  console.log("Responsive Header")
+
+  return (
+    <header className="responsive-header">
+      <h1 className="logo logo-long"><NavLink to="/" exact={true}>Clinton Hill Fort Greene Mutual Aid</NavLink></h1>
+      <h1 className="logo logo-short"><NavLink to="/" exact={true}>Mutual Aid</NavLink></h1>
+      <input type="checkbox" id="nav-toggle" className="nav-toggle"/>
+      <nav>
+        <ul>
+          <li><NavLink to="/" exact={true} className="about-link" activeClassName='is-active'>About</NavLink></li>
+          <li><NavLink to="/get-help" className="get-help-link" activeClassName='is-active'>Get Help</NavLink></li>
+          <li><NavLink to="/give-help" className="give-help-link" activeClassName='is-active'>Give Help</NavLink></li>
+          <li><NavLink to="/donate" className="donate-link" activeClassName='is-active'>Donate</NavLink></li>
+          {/* <li><NavLink to="/search" className="link"><img src={magnifyingGlassSVG} alt="search icon"/></NavLink></li> */}
+        </ul>
+      </nav>
+      <label for="nav-toggle" className="nav-toggle-label">
+        <span></span>
+      </label>
+    </header>
+  )
+}
+
+export default ResponsiveHeader;
+
 function Header() {
   console.debug("Header")
   const location = useLocation();
   const [styles, setStyles] = useState(getColorScheme(location.pathname))
-  
+
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
     console.log("EFFECT - location", location)
     setStyles(getColorScheme(location.pathname))
   }, [location])
-  
+
   return (
     <header className={`header`} style={{
       backgroundColor: styles.primary
@@ -27,9 +53,9 @@ function Header() {
           <li className="home"><NavLink to="/" exact={true}>Clinton Hill Fort Greene Mutual Aid</NavLink></li>
           <li className="link"><NavLink to="/" exact={true} className="about-link" activeClassName='is-active'>About</NavLink></li>
           <li className="link"><NavLink to="/get-help" className="get-help-link" activeClassName='is-active'>Get Help</NavLink></li>
-          <li className="link"><NavLink to="/give-help"  className="give-help-link" activeClassName='is-active'>Give Help</NavLink></li>
+          <li className="link"><NavLink to="/give-help" className="give-help-link" activeClassName='is-active'>Give Help</NavLink></li>
           <li className="link"><NavLink to="/donate" className="donate-link" activeClassName='is-active'>Donate</NavLink></li>
-          <li className="link"><NavLink to="/" className="link"><img src={magnifyingGlassSVG} alt="search icon" style={{margin: "7px 0 0 0"}}/></NavLink></li>
+          <li className="link"><NavLink to="/" className="link"><img src={magnifyingGlassSVG} alt="search icon" style={{ margin: "7px 0 0 0" }} /></NavLink></li>
         </ul>
       </nav>
     </header>
@@ -42,7 +68,9 @@ Header.propTypes = {
   }),
 }
 
-export default  Header;
+
+
+
 
 function getColorScheme(pathname) {
   console.log("getColorScheme :::", pathname)
