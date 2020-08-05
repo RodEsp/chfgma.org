2f7a41b89f5ded0b82ca93766aede67800b20c15
William Heiderman <heiderman.william@gmail.com>
2020-08-05 14:57:59 -0400

    - changed nav background opacity - added button hover animation

--- src/components/layout/buttons/ButtonLink.js
+++ src/components/layout/buttons/ButtonLink.js
@@ -5,33 +5,10 @@ import { Link } from 'react-router-dom';
 function ButtonLink({linkTo, text, styles}) {
     // console.log("ButtonLink :::", styles)
     return (
-        <Link style={{...buttonLinkStyle, ...styles}} to={linkTo}>{text} {buttonChevron}</Link>
+        <Link className="button-link" style={{...styles}} to={linkTo}>{text}</Link>
     )
 }
-const buttonChevron = <Chevron style={{ marginLeft: "10px" }} />
-
-// Styles
-const buttonLinkStyle = {
-    display: "inline-block",
-    // flex: "auto 1 auto",
-    flex: "1",
-
-    margin: "2px",
-    padding: "9px 25px",
-    boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)",
-    border: "none",
-    borderRadius: "100px",
-
-    color: "#FFFFFF",
-    fontFamily: "Montserrat",
-    fontStyle: "normal",
-    fontWeight: "bold",
-    fontSize: "18px",
-    lineHeight: "22px",
-    textAlign: "center",
-    whiteSpace: "nowrap",
-    
-}
+const buttonChevron = <Chevron className="chevron" style={{ marginLeft: "10px" }} />
 
 export { ButtonLink };
 
