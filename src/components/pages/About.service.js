cb4d9b25c4b69a711b9f650ac950dd84fe295883
William Heiderman <heiderman.william@gmail.com>
2020-08-07 11:58:45 -0400

    responsive styles

--- src/components/pages/About.service.js
+++ src/components/pages/About.service.js
@@ -6,17 +6,41 @@ import iconMessagesIMG from "../../assets/icons/icon-message.png";
 export const ourPrinciples = [{ title: "Principle ipsum lorem ipsum dolor sit amet", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." }, { title: "Principle ipsum lorem ipsum dolor sit amet", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." }, { title: "Principle ipsum lorem ipsum dolor sit amet", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." }, { title: "Principle ipsum lorem ipsum dolor sit amet", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." }, { title: "Principle ipsum lorem ipsum dolor sit amet", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." }];
 
 export const stayUpToDateCards = [{
-        image: iconMailboxIMG,
-        alt: "mailbox icon",
-        text: "Subscribe to our weekly newsletter",
-    }, {
-        image: iconMessagesIMG,
-        alt: "message icon",
-        text: "Join the conversation on Slack",
-    }, {
-        image: iconPicIMG,
-        alt: "picture icon",
-        text: "Follow us on Instagram for updates",
-    }];
+    image: iconMailboxIMG,
+    alt: "mailbox icon",
+    text: "Subscribe to our weekly newsletter",
+}, {
+    image: iconMessagesIMG,
+    alt: "message icon",
+    text: "Join the conversation on Slack",
+}, {
+    image: iconPicIMG,
+    alt: "picture icon",
+    text: "Follow us on Instagram for updates",
+}];
 
-// export default  {ourPrinciples, stayUpToDateCards};
+export const stats = [{
+    stat: "$44,997",
+    text: "total contributed to our Mutual Aid Fund and Direct Action Funds",
+}, {
+    stat: "10,000",
+    text: "grocery packages delivered through our weekly collaboration with food pantries",
+}, {
+    stat: "658",
+    text: "requests fulfilled for groceries and essential supplies",
+}, {
+    stat: "135+",
+    text: "people attended training sessions",
+}, {
+    stat: "533",
+    text: "callbacks to requests on our hotline",
+}, {
+    stat: "988",
+    text: "people have joined our Slack community",
+}, {
+    stat: "948",
+    text: "wellness check calls made",
+}, {
+    stat: "760",
+    text: "people have followed our Instagram account",
+}]
