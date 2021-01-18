63e4351f876bbee9dc4daaf0e290aeaeb7a3cc16
William Heiderman <heiderman.william@gmail.com>
2021-01-18 13:55:16 -0500

    * updates give help page for desktop

--- src/components/pages/GiveHelp.js
+++ src/components/pages/GiveHelp.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import "./GiveHelp.css";
-import flyerPreviewIMG from "../../assets/flyer-preview.png"
+import peopleMakingDonationsIMG from "../../assets/people-making-donations.png"
+import newsletterIMG from "../../assets/newsletter.png"
 import pullingBoxesIMG from "../../assets/pulling-boxes.png"
+import config from "../../config.js"
+
+const {
+    MUTUAL_AID_INSTAGRAM_FRIDGE_LINK
+} = config;
 
 function GiveHelp() {
     return (
@@ -9,58 +15,16 @@ function GiveHelp() {
             <section className="jumbo-title">Join our growing community</section>
             <Help />
             <Stats />
-            {/* <MoreInfo /> */}
+            <FoodSecurityAndWorkingGroups />
+            <HelpUsSpreadTheWord />
         </div>
-    )
-}
-
-function MoreInfo() {
-    const HowYouCanHelp = () => (
-        <div className="text-group">
-            <h1 className="header1">How you can help</h1>
-            <ul>
-                <li>Shop for groceries and essential supplies for people isolating at home</li>
-                <li>Provide transportation for making deliveries of groceries and essential supplies</li>
-                <li>Create connections with neighbors and neighborhood groups</li>
-                <li>Donate gently-used goods like mattresses, electronics, clothing, and other necessities</li>
-            </ul>
-        </div>
-    )
-    return (
-        <section className="more-info two-in-one">
-            <div className="row-container">
-                <HowYouCanHelp />
-                <img className="pulling-boxes-img" src={pullingBoxesIMG} alt="image of person pulling boxes" />
-                <div className="fancy-list-wrapper">
-                    <h1 className="header1">New to Mutual Aid?</h1>
-                    <div>
-                        <div className="fancy-list-container standard-text">
-                            <span><b>01</b></span>
-                            {/* TODO: Is this a link? */}
-                            <span>Learn what <span className="text-important">mutual aid</span> means (we practice solidarity, not charity)</span>
-                        </div>
-                        <div className="fancy-list-container standard-text">
-                            <span><b>02</b></span>
-                            {/* TODO: Is this a link? */}
-                            <span>Take a minute to fill out our <span className="text-important">volunteer intake form</span></span>
-                        </div>
-                        <div className="fancy-list-container standard-text">
-                            <span><b>03</b></span>
-                            {/* TODO: Is this a link? */}
-                            <span>Join us on <span className="text-important">Slack</span> to organize with other volunteers</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
     )
 }
 
 function Help() {
-    const communityFridgeLink = <a href="/get-help" alt="community fridge link">Community Fridge</a>
-    const HowYouCanHelp = () => (
-        <div className="text-group">
-            <h1 className="header1">How you can help</h1>
+    const communityFridgeLink = <a href="/get-help" alt="community fridge link" className="img">Community Fridge</a>,
+        HowYouCanHelp = () => <div className="section-1 text-group">
+            <div className="header1">How you can help</div>
             <ul>
                 <li>Shop for groceries and essential supplies for people isolating at home</li>
                 <li>Help clean and stock our {communityFridgeLink}</li>
@@ -68,36 +32,35 @@ function Help() {
                 <li>Create connections with neighbors and neighborhood groups</li>
                 <li>Donate gently-used goods like mattresses, electronics, clothing, and other necessities</li>
             </ul>
-        </div>
-    )
-    return (
-        <section className="help two-in-one">
-            <div className="row-container">
-                <HowYouCanHelp />
-                <img className="pulling-boxes-img" src={pullingBoxesIMG} alt="image of person pulling boxes" />
-                <div className="fancy-list-wrapper">
-                    <h1 className="header1">New to Mutual Aid?</h1>
-                    <div>
-                        <div className="fancy-list-container standard-text">
-                            <span><b>01</b></span>
-                            {/* TODO: Is this a link? */}
-                            <span>Learn what <span className="text-important">mutual aid</span> means (we practice solidarity, not charity)</span>
-                        </div>
-                        <div className="fancy-list-container standard-text">
-                            <span><b>02</b></span>
-                            {/* TODO: Is this a link? */}
-                            <span>Take a minute to fill out our <span className="text-important">volunteer intake form</span></span>
-                        </div>
-                        <div className="fancy-list-container standard-text">
-                            <span><b>03</b></span>
-                            {/* TODO: Is this a link? */}
-                            <span>Join us on <span className="text-important">Slack</span> to organize with other volunteers</span>
-                        </div>
-                    </div>
+        </div>,
+        List = () => <div className="fancy-list-wrapper section-2">
+            <div className="header1">New to Mutual Aid?</div>
+            <div>
+                <div className="fancy-list-container standard-text">
+                    <span><b>01</b></span>
+                    {/* TODO: Is this a link? */}
+                    <span>Learn what <span className="text-important">mutual aid</span> means (we practice solidarity, not charity)</span>
+                </div>
+                <div className="fancy-list-container standard-text">
+                    <span><b>02</b></span>
+                    {/* TODO: Is this a link? */}
+                    <span>Take a minute to fill out our <span className="text-important">volunteer intake form</span></span>
+                </div>
+                <div className="fancy-list-container standard-text">
+                    <span><b>03</b></span>
+                    {/* TODO: Is this a link? */}
+                    <span>Join us on <span className="text-important">Slack</span> to organize with other volunteers</span>
                 </div>
             </div>
-        </section>
-    )
+        </div>
+
+    return <section className="how-you-can-help standard-text">
+        <div className="row-container">
+            <HowYouCanHelp />
+            <img className="pulling-boxes-img" src={pullingBoxesIMG} alt="image of person pulling boxes" />
+            <List />
+        </div>
+    </section>
 }
 
 function Stats() {
@@ -126,6 +89,54 @@ function Stats() {
             </div>
         </section>
     )
+}
+
+function FoodSecurityAndWorkingGroups() {
+    const FoodSecurity = () => <div className="food-security section-1">
+        <div className="header1">Food security</div>
+        <div className="title">Addressing food security is a key part of our mutual aid effort. Here’s how you can help:</div>
+        <ol>
+            <li><b>Grocery shop for a neighbor:</b> Volunteer to take a one-time request, or sign up to be someone’s weekly shopper. You can be reimbursed from our Groceries & Essentials Fund.</li>
+            <li><b>Help stock and/or clean our Community Fridge:</b> Help stock and/or clean our Community Fridge: You can stop by and donate food any time, or sign up to help clean and organize the fridge regularly. Follow <a href={MUTUAL_AID_INSTAGRAM_FRIDGE_LINK} alt="mutual aid fridge instagram link">@fgcommunityfridge</a> to learn more.</li>
+            <li><b>Support weekly food pantry distributions:</b> We help with large scale food pantry and meal deliveries in collaboration with community organizations, which benefit 1,000+ families each week.</li>
+        </ol>
+    </div>
+
+    const WorkingGroups = () => <div className="working-groups section-2">
+        <div className="header1">Working groups</div>
+        <div>Our working groups coordinate on specific focuses within our mutual aid effort. Anyone is welcome to join and contribute in whatever capacity they can. Some of our active working groups include:</div>
+        <ul>
+            <li>Advocacy</li>
+            <li>Community Building</li>
+            <li>Community Fridge</li>
+            <li>Design</li>
+            <li>Fundraising</li>
+            <li>Intake</li>
+            <li>Tech Support</li>
+        </ul>
+    </div>
+
+    return <section className="food-security-and-working-groups">
+        <div className="row-container standard-text two-in-one-v2">
+            <FoodSecurity />
+            <img className="people-making-donations-img img" src={peopleMakingDonationsIMG} alt="people making donations" />
+            <WorkingGroups />
+        </div>
+    </section>
+}
+
+function HelpUsSpreadTheWord() {
+    return <section className="help-us-spread-the-word">
+        <div className="row-container standard-text split-wrapper">
+            <div>
+                <div className="header1">Help us spread the word</div>
+                <div>
+                    <div>If you have access to a printer, print and share our flyers, or make one of your own. Flyering your building/street/block is key to connecting those in need to our resources.<br/><br/>Our flyer is available in English, Spanish, and Chinese. Download all three in one PDF here.</div>
+                </div>
+            </div>
+            <img src={newsletterIMG} alt="newsletter" />
+        </div>
+    </section>
 }
 
 
--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 // TODO: get these from env variables
 // TODO: Get values of MUTUAL_AID_NEWSLETTER_SUBSCRIBE_LINK, MUTUAL_AID_SLACK_LINK, MUTUAL_AID_INSTAGRAM_LINK, MUTUAL_AID_FACEBOOK_LINK
 
+const MUTUAL_AID_NYC_LINK = "https://mutualaid.nyc/"
 const MUTUAL_AID_NEWSLETTER_SUBSCRIBE_LINK = "https://notion.us19.list-manage.com/subscribe?u=a2f7efb31fe0c09ee5856bacb&id=9db6a5352a";
 const MUTUAL_AID_SLACK_LINK = "https://chfgmutualaid.slack.com/join/shared_invite/zt-dol9r5tu-Af~7Y5IIpCW17oOL~wBa1A#/";
 const MUTUAL_AID_PHONE_NUMBER_COUNTRY_CODE = "+1",
@@ -25,7 +26,8 @@ const config = {
     MUTUAL_AID_AIRTABLE_LINK,
     MUTUAL_AID_INSTAGRAM_FRIDGE_LINK,
     MUTUAL_AID_FRIDGE_LOCATION_LINK,
-    COVID_TEXTING_HOTLINE
+    COVID_TEXTING_HOTLINE,
+    MUTUAL_AID_NYC_LINK
 }
 
 export default config;
