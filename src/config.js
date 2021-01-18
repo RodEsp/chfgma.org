0b19521b663119cccba86acaa2e171c542c1c0e6
William Heiderman <heiderman.william@gmail.com>
2021-01-18 14:51:21 -0500

    * updates donate page for desktop

--- src/config.js
+++ src/config.js
@@ -1,18 +1,21 @@
 // TODO: get these from env variables
 // TODO: Get values of MUTUAL_AID_NEWSLETTER_SUBSCRIBE_LINK, MUTUAL_AID_SLACK_LINK, MUTUAL_AID_INSTAGRAM_LINK, MUTUAL_AID_FACEBOOK_LINK
 
-const MUTUAL_AID_NYC_LINK = "https://mutualaid.nyc/"
-const MUTUAL_AID_NEWSLETTER_SUBSCRIBE_LINK = "https://notion.us19.list-manage.com/subscribe?u=a2f7efb31fe0c09ee5856bacb&id=9db6a5352a";
-const MUTUAL_AID_SLACK_LINK = "https://chfgmutualaid.slack.com/join/shared_invite/zt-dol9r5tu-Af~7Y5IIpCW17oOL~wBa1A#/";
-const MUTUAL_AID_PHONE_NUMBER_COUNTRY_CODE = "+1",
-      MUTUAL_AID_PHONE_NUMBER = "862-277-0747";
-const MUTUAL_AID_EMAIL_ADDRESS_HELLO = "hello@chfgma.org";
-const MUTUAL_AID_INSTAGRAM_LINK = "https://www.facebook.com/chfgaid/",
-      MUTUAL_AID_FACEBOOK_LINK = "https://www.instagram.com/chfgaid/";
-const MUTUAL_AID_AIRTABLE_LINK = "https://airtable.com/shrAwewkpHBHWyh5L/tbl4xknvSOLNB7bcI";
-const MUTUAL_AID_INSTAGRAM_FRIDGE_LINK = "https://www.instagram.com/fgcommunityfridge/";
-const MUTUAL_AID_FRIDGE_LOCATION_LINK="https://www.google.com/maps/place/357+Myrtle+Ave,+Brooklyn,+NY+11205/@40.6847488,-73.97376,14z/data=!4m5!3m4!1s0x89c25bb7f11a68b7:0xca08dde714414b59!8m2!3d40.6934011!4d-73.9717401"
-const COVID_TEXTING_HOTLINE = "692-692"
+export const MUTUAL_AID_NYC_LINK = "https://mutualaid.nyc/"
+export const MUTUAL_AID_NEWSLETTER_SUBSCRIBE_LINK = "https://notion.us19.list-manage.com/subscribe?u=a2f7efb31fe0c09ee5856bacb&id=9db6a5352a";
+export const MUTUAL_AID_SLACK_LINK = "https://chfgmutualaid.slack.com/join/shared_invite/zt-dol9r5tu-Af~7Y5IIpCW17oOL~wBa1A#/";
+export const MUTUAL_AID_PHONE_NUMBER_COUNTRY_CODE = "+1",
+    MUTUAL_AID_PHONE_NUMBER = "862-277-0747";
+export const MUTUAL_AID_EMAIL_ADDRESS_HELLO = "hello@chfgma.org";
+export const MUTUAL_AID_INSTAGRAM_LINK = "https://www.facebook.com/chfgaid/",
+    MUTUAL_AID_FACEBOOK_LINK = "https://www.instagram.com/chfgaid/";
+export const MUTUAL_AID_AIRTABLE_LINK = "https://airtable.com/shrAwewkpHBHWyh5L/tbl4xknvSOLNB7bcI";
+export const MUTUAL_AID_INSTAGRAM_FRIDGE_LINK = "https://www.instagram.com/fgcommunityfridge/";
+export const MUTUAL_AID_FRIDGE_LOCATION_LINK = "https://www.google.com/maps/place/357+Myrtle+Ave,+Brooklyn,+NY+11205/@40.6847488,-73.97376,14z/data=!4m5!3m4!1s0x89c25bb7f11a68b7:0xca08dde714414b59!8m2!3d40.6934011!4d-73.9717401"
+export const COVID_TEXTING_HOTLINE = "692-692"
+
+export const IOBY_LINK = "https://ioby.org/project/clinton-hill-fort-greene-mutual-aid"
+export const DONORBOX_LINK = "https://donorbox.org/chfgma"
 
 
 const config = {
@@ -27,7 +30,9 @@ const config = {
     MUTUAL_AID_INSTAGRAM_FRIDGE_LINK,
     MUTUAL_AID_FRIDGE_LOCATION_LINK,
     COVID_TEXTING_HOTLINE,
-    MUTUAL_AID_NYC_LINK
+    MUTUAL_AID_NYC_LINK,
+    IOBY_LINK,
+    DONORBOX_LINK
 }
 
 export default config;
